23dc144991a5d5b7650abf8ae54d11bc9886ff9e
mikepatrick <mpatrick@cds-global.com>
2024-04-20 10:02:28 -0500

    remove unused scripts

--- .github/workflows/check.sh
+++ .github/workflows/check.sh
@@ -1,3 +1,30 @@
-hash1=$(git show-ref --heads -s origin/main)
-hash2=$(git merge-base origin/main $GITHUB_HEAD_REF)
-[ "${hash1}" = "${hash2}" ] && echo "OK" || exit 1
+#!/bin/bash
+# Usage:
+#       check_rebase.sh [current branch] [integration branch]
+
+current_branch=$1
+target_branch=$2
+
+git fetch origin main:main
+git fetch origin $GITHUB_HEAD_REF:$GITHUB_HEAD_REF
+
+if [[ -z $current_branch ]]; then
+    trg=HEAD
+fi
+if [[ -z $itg ]]; then
+    target_branch=main
+fi
+
+ret=$(git rev-list $current_branch..$target_branch)
+if [[ -z $ret ]]; then
+    echo "PR branch up to date with target."
+    echo "No rebase required."
+else
+    echo "PR branch is behind target branch."
+    echo "Rebase required to absorb the following changes:"
+    for r in $ret
+    do
+        echo $r
+    done
+    exit 1
+fi
