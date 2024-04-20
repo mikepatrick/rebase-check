04e908e3a5c9e6eadeead8f66324206540f671aa
mikepatrick <mpatrick@cds-global.com>
2024-04-20 09:27:26 -0500

    fix: avoid checkout step

--- check2.sh
+++ check2.sh
@@ -1,4 +1,5 @@
 git fetch origin main:main
+git fetch origin $GITHUB_HEAD_REF:$GITHUB_HEAD_REF
 hash1=$(git show-ref --heads -s main)
 hash2=$(git merge-base main $GITHUB_HEAD_REF)
 if [ "${hash1}" = "${hash2}" ]
